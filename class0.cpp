037cf5370f612ed4fa1f0babc5efc1a99cca95e7
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2023-09-08 01:07:45 +0530

    08-09-2023
    This is a program of accessing an object through classes.

---
+++ class0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Adarsh{
+	public:
+	string name;
+	float tenthmark;
+	float twelthmark;
+	
+};
+int main(){
+	Adarsh A1;
+	cin>>A1.name>>A1.tenthmark>>A1.twelthmark;
+	cout<<A1.name<<endl;
+	cout<<A1.tenthmark<<endl;
+	cout<<A1.twelthmark;
+	return 0;
+}
