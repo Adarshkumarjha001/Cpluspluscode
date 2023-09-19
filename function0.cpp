7c932e14976b3d6774b6446f14c54ffe5da4d2d9
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2023-09-19 21:38:26 +0530

    19-09-2023
    This is a program of function in computer.

---
+++ function0.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void my_information(string name,double marks){
+	
+	cout<<"My name is "<<name<<" And i got "<<marks<<" in my 12th class";
+
+}
+int main(){
+	
+	my_information("Adarsh Kumar Jha",96.40);
+	return 0;
+	
+}
