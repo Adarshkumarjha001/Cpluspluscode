f7cc03704b51004270f7e91fc6936d783f5a4964
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2023-09-25 18:28:42 +0530

    25-09-2023
    Problem 0.

---
+++ prob_0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=50;
+	int b=0;
+	int c=35;
+	while(b<a)
+{
+	if(c<=0||c>50){
+		cout<<"Adarsh"<<endl;
+		continue;
+	}
+	b++;
+	
+}
+cout<<"sake";
+return 0;
+}
